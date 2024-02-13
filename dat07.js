2ac3f3f0090e29d35ecd02f311384837236a1153
pravinkuyate <pravinkuyate0@gmail.com>
2024-02-13 16:51:45 +0530

    commit day 7

---
+++ dat07.js
@@ -0,0 +1,13 @@
+let a=10;
+let b=10;
+
+console.log("== is",a==b)
+console.log("===",a===b)
+let c =10;
+let d ="10";
+console.log("== is",c==d)
+console.log("===",c===d)
+
+
+console.log(!false)
+
